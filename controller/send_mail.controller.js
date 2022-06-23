e3cc1f2179d782d185c52833809d762ccfaa9732
seifeddineelbehi <elbehiseifeddine@gmail.com>
2022-06-23 17:40:29 +0100

    fix port

--- controller/send_mail.controller.js
+++ controller/send_mail.controller.js
@@ -1,5 +1,18 @@
 const nodemailer = require("nodemailer");
 let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        type: 'oauth2',
+        user: "ifest.app.help@gmail.com", // Your gmail address.
+        clientId: "387800876011-perqhqt6kdrka292n49g5urk33vpvu1l.apps.googleusercontent.com",
+        clientSecret: "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",
+        refreshToken: "1//04CGhf7nz83lwCgYIARAAGAQSNwF-L9IryQxAG4n3ZOcSgd_pEDE6lF6EnKPxCpadNK8DLUlg17X9-JiFhdaQ4dVItQmJQ8rv4bY",
+    }
+});
+
+/*nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
@@ -8,8 +21,7 @@ let transporter = nodemailer.createTransport({
         user: "Ifest.app.help@gmail.com",
         accessToken: "ya29.a0ARrdaM92pEDSL3EqCGyAjtoTN0oGHVm0RFDCX0SRnHBVAHeIONNekQpPWxXWGIjMLig26YhHSYuKSir7tAyXGqs6kQ3lojqT2GdfT9w351SyMj7VTt910lYrgheZ6W37iQK6X369dD2RPP10rAfsMZdx9z_-",
     },
-});
-
+});*/
 
 module.exports = {
 
@@ -27,7 +39,7 @@ module.exports = {
 
         var message = {
             from: 'Ifest.app.help <Ifest.app.help@gmail.com>',
-            to: "info.atast@gmail.com",
+            to: "seifeddineelbehi@gmail.com",
             subject: req.body.emailType,
             html: body,
 
