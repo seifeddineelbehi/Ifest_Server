9c504f8956c0bfd98a41befbc388ed7fbe6ffad8
AhmedDhm <ahmed.dahman24@gmail.com>
2023-02-11 14:03:26 +0100

    fix general planning

--- controller/general_palanning.controller.js
+++ controller/general_palanning.controller.js
@@ -7,75 +7,82 @@ module.exports = {
         var planing = req.body.planing;
         const dateString = req.body.date;
         const date = new Date(dateString);
-        if (req.admin.username != null){
-          const general = new GeneralPlanning({date : date});
-    
-          var listPlanning = [];
-          planing.forEach(async element => {
-            const eventPlaning = new EventPlaning({
-              element,
-            });
-            await eventPlaning.save();
-            listPlanning.push(eventPlaning);
-          });
-    
-          general.planing.push(...listPlanning);
-          await general.save();
-          res.status(200).send({
-            success: true,
-            message: "General plannig added successfully!",
-            generalPlanning: general,
-          });
-        }else {
-          res.status(401).send({
-            success: false,
-            message: "Unauthorized!",
-          });
-        }
-        
-      },
+        try {
+            if (req.admin.username != null) {
+                const general = new GeneralPlanning({ date: date });
 
-      getGeneralPlanningByWeek : async (req, res) => {
+                var listPlanning = [];
+                planing.forEach(async element => {
+                    const eventPlaning = new EventPlaning({
+                        element,
+                    });
+                    await eventPlaning.save();
+                    listPlanning.push(eventPlaning);
+                });
+
+                general.planing.push(...listPlanning);
+                await general.save();
+                res.status(200).send({
+                    success: true,
+                    message: "General plannig added successfully!",
+                    generalPlanning: general,
+                });
+            } else {
+                res.status(401).send({
+                    success: false,
+                    message: "Unauthorized!",
+                });
+            }
+        } catch (error) {
+            res.status(500).send({
+                success: false,
+                message: error,
+            });
+        }
+
+    },
+
+    getGeneralPlanningByWeek: async (req, res) => {
         const startOfWeek = moment().startOf('week').toDate();
         const endOfWeek = moment().endOf('week').toDate();
         try {
             var listGeneralPlanning = await GeneralPlanning.find({ date: { $gte: startOfWeek, $lte: endOfWeek } }).populate([
                 {
-                  path: "planing",
-                  model: "EventPlaning",
+                    path: "planing",
+                    model: "EventPlaning",
                 },
-              ]); 
-              res.status(200).send({
+            ]);
+            res.status(200).send({
                 success: true,
                 listGeneralPlanning: listGeneralPlanning,
-              });
+            });
         } catch (error) {
             res.status(500).send({
                 success: false,
                 message: error,
-              });
+            });
         }
-          
+
     },
 
     getAllGeneralPlanning: async (req, res) => {
         try {
             var listGeneralPlanning = await GeneralPlanning.find().populate([
                 {
-                  path: "planing",
-                  model: "EventPlaning",
+                    path: "planing",
+                    model: "EventPlaning",
                 },
-              ]); 
-              res.status(200).send({
+            ]);
+            res.status(200).send({
                 success: true,
                 listGeneralPlanning: listGeneralPlanning,
-              });
+            });
         } catch (error) {
             res.status(500).send({
                 success: false,
                 message: error,
-              });
+            });
         }
-          
+
     },
 }
