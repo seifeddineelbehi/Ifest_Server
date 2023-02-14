002cdb8205475cdfd7a22fce08e3339d50f5044a
seifeddineelbehi <elbehiseifeddine@gmail.com>
2023-02-14 14:59:52 +0100

    change

--- controller/events.controller.js
+++ controller/events.controller.js
@@ -1,27 +1,20 @@
-const Events = require("../models/events.model");
-const EventPlaning = require("../models/event_planing.model");
+const { Events } = require("../models/events.model");
+const { EventPlaning } = require("../models/event_planing.model");
 const fs = require("fs");
 
 
 async function SetupEventFolder(id) {
-  const dir = `./uploads/Events`;
+  const dir = `./uploads/Events/Event-${id}`;
 
   fs.mkdir(dir, function () {
-    fs.exists(dir, function (exist, err) {
-      if (exist) {
-        const dir2 = `./uploads/Events/Event-${id}`;
-        fs.mkdir(dir2, function () {
-          fs.exists(dir2, function (exist, err) {
-            if (exist) {
-              const dir3 = `./uploads/Events/Event-${id}/images`;
-              fs.mkdir(dir3, function () {
-                console.log("folder created");
+      fs.exists(dir, function (exist, err) {
+          if (exist) {
+              const dir2 = `./uploads/Events/Event-${id}/images`;
+              fs.mkdir(dir2, function () {
+                  console.log("folder created");
               });
-            }
-          });
-        });
-      }
-    });
+          }
+      });
   });
 };
 
@@ -32,32 +25,32 @@ module.exports = {
     const eventId = req.headers.idevent;
     try {
 
-      console.log("uploaded");
-      //console.log(req.file);
-      var pathImage = req.file.path;
-      pathImage = pathImage.replaceAll("\\", "/");
-      const uploadEvent = await Events.updateOne(
-        { _id: eventId },
-        {
-          $set: {
-            image: pathImage,
-          },
-        }
-      );
-      res.status(200).send({
-        success: true,
-        message: "image uploaded",
-        uploadEvent: uploadEvent
-      });
+        console.log("uploaded");
+        //console.log(req.file);
+        var pathImage = req.file.path;
+        pathImage = pathImage.replaceAll("\\", "/");
+        const uploadEvent = await Events.updateOne(
+            { _id: eventId },
+            {
+                $set: {
+                    image: pathImage,
+                },
+            }
+        );
+        res.status(200).send({
+            success: true,
+            message: "image uploaded",
+            uploadEvent: uploadEvent
+        });
 
     } catch (error) {
-      res.status(304).send({
-        success: false,
-        message: "error upload image",
-        error: error
-      });
+        res.status(304).send({
+            success: false,
+            message: "error upload image",
+            error: error
+        });
     }
-  },
+},
 
   addEvent: async (req, res) => {
     const {
@@ -69,49 +62,37 @@ module.exports = {
       eventAbout,
       planing,
     } = req.body;
+    console.log(planing);
 
-
-    if (req.user.username != null) {
+    if (req.user.username != null){
       const event = new Events({
-        eventName,
-        eventLocation,
-        eventPrice,
-        eventDate,
-        eventTime,
-        eventAbout,
+        ...req.body,
       });
 
       var listPlanning = [];
-
-      for (let index = 0; index < planing.length; index++) {
-
-        const element = planing[index];
-
-        const eventPlaning = new EventPlaning({ ...element });
-
+      planing.forEach(async element => {
+        const eventPlaning = new EventPlaning({
+          element,
+        });
         await eventPlaning.save();
-
         listPlanning.push(eventPlaning);
-      }
-
-
+      });
 
       event.planing.push(...listPlanning);
-      const newEvent = await event.save();
-      console.log(newEvent._id);
-      SetupEventFolder(newEvent._id);
+      await event.save();
+      SetupEventFolder(event._id);
       res.status(200).send({
         success: true,
         message: "Event added successfully!",
         eventDetails: event,
       });
-    } else {
+    }else {
       res.status(401).send({
         success: false,
         message: "Unauthorized!",
       });
     }
-
+    
   },
 
   getAllEvents: async (req, res) => {
@@ -123,9 +104,6 @@ module.exports = {
         },
       ])
       .sort({ createdAt: -1 });
-    return res.status(200).send({
-      success: true,
-      list: events,
-    });
+    return res.status(200).send( events);
   },
 };
